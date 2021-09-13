644025129566c8e146e848c21e4688ac6db58347
vineetson <32320240+vineetson@users.noreply.github.com>
2021-09-13 17:09:07 +0530

    Add files via upload

---
+++ SearchInRange.java
@@ -0,0 +1,30 @@
+package com.vineet;
+
+import java.util.Arrays;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, -7, 3, 14, 28};
+        int target = 14;
+        int ans = linearSearch(arr,target,1,4);
+        System.out.println(ans);
+    }
+
+    static int linearSearch(int[] arr, int target, int start, int end){
+        if (arr.length == 0){
+            return -1;
+        }
+
+        // now run a for loop
+        for (int index = start; index <= end; index++) {
+            // check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target){
+                return index;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return -1;
+    }
+}
